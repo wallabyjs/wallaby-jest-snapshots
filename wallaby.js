83483d37c7f8d5cef1a345833bc39c3275e23e25
Artem Govorov <artem.govorov@gmail.com>
2017-11-02 13:24:09 +1000

    Restored js example

--- wallaby.js
+++ wallaby.js
@@ -1,17 +1,21 @@
 module.exports = function (wallaby) {
   return {
     files: [
-      'src/**/*.ts?(x)',
+      'src/**/*.js?(x)',
       'src/**/*.snap',
-      '!src/**/*.spec.ts?(x)'
+      '!src/**/*.spec.js?(x)'
     ],
     tests: [
-      'src/**/*.spec.ts?(x)'
+      'src/**/*.spec.js?(x)'
     ],
 
     env: {
       type: 'node',
       runner: 'node'
+    },
+
+    compilers: {
+      "**/*.js?(x)": wallaby.compilers.babel()
     },
 
     testFramework: 'jest',
